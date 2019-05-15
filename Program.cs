8d386fd3f06a51f4634e4060130384060c96fb98
Attila Kling <attila.kling@timelinerx.com>
2019-05-15 17:01:29 +0200

    First change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace dungeonseeker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Szivem!");
         }
     }
 }
